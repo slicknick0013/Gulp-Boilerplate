e737787cfd1ab91553da50a56e72d1d6f27f858f
Matthew Turnure <matt@mattturnure.com>
2015-01-27 13:59:06 -0500

    added some js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,2 +1,29 @@
-/* global console */
-console.log('main.js loaded');
+/* global console, window, document */
+
+var main = (function (global, doc) {
+    'use strict';
+
+    var self = this;
+
+    return {
+        react: function (action, func) {
+            var behavior = action || 'click',
+                fun      = func || function () {};
+            if (typeof addEventListener === 'function') {
+                doc.body.addEventListener(behavior, fun, false);
+            }
+        },
+
+        externalLinks: function (e) {
+            if (e.target.rel === 'external') {
+                e.preventDefault();
+                open(e.target.href);
+                return false;
+            }
+
+            return;
+        }
+    };
+}(window, document));
+
+main.react('click', main.externalLinks);
